42506bc280e24aef31b8b293fc6d2bc7fb48821f
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-08-29 16:55:16 +0200

    Create React App + Nav

---
+++ netflix-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNav from './components/CustomNav';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomNav />
+      
+    </div>
+  );
+}
+
+export default App;
