754d5f48064dd78cf5f00d6cbaf4e8d95a3aac58
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-08-31 16:54:45 +0200

    Cloned repo & changed bearer token

---
+++ m4d7-hooks/src/components/SingleComment.jsx
@@ -0,0 +1,28 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const deleteComment = async (asin) => {
+    try {
+        let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin, {
+            method: 'DELETE',
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZmJmMDJkNTI2MjAwMTViNmRjYWEiLCJpYXQiOjE2MzAwMTAwODMsImV4cCI6MTYzMTIxOTY4M30.IYE6DU5jYldk8SdSocMrRqffiucDB0xBM0HT5k_OU3w"
+            }
+        })
+        if (response.ok) {
+            alert('comment deleted!')
+        } else {
+            alert('comment NOT deleted!')
+        }
+    } catch (error) {
+        alert('comment NOT deleted!')
+    }
+}
+
+const SingleComment = ({ comment }) => (
+    <ListGroup.Item>
+        {comment.comment}
+        <Button variant="danger" className="ml-2" onClick={() => deleteComment(comment._id)}>D</Button>
+    </ListGroup.Item>
+)
+
+export default SingleComment
